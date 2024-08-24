c3ccd3a879aac307ff48aa316f8436c3f2e17ee2
Augment Chinedu <augmentchinedu@gmail.com>
2024-08-24 08:18:27 +0100

    New Build. Github Connected to Discord Server. Profile Page Updated. Postpage Creation Initialized

--- core/index.js
+++ core/index.js
@@ -1,17 +1,56 @@
 const services = require("../classes/services");
 const books = require("../classes/books");
-async function init() {
-  const { Octokit } = await import("octokit");
+const { Server } = require("ws");
+
+async function getReportFromLatestCommit() {
+  const { Octokit } = await import("@octokit/rest");
 
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-  const data = await octokit.request("GET /repos/{owner}/{repo}", {
-    owner: process.env.REPO_OWNER,
-    repo: "kingchat.one",
-  });
-  console.log("git connected");
+  try {
+    const { frontendData } = await octokit.repos.listCommits({
+      owner: process.env.REPO_OWNER,
+      repo: "kingchat",
+      sha: "main",
+      per_page: 1,
+    });
+
+    const { backendData } = await octokit.repos.listCommits({
+      owner: process.env.REPO_OWNER,
+      repo: "kingchat.one",
+      sha: "main",
+      per_page: 1,
+    });
+    const client = frontendData[0];
+    const server = backendData[0];
+
+    const report = {
+      client: {
+        committer: client.commit.author.name,
+        message: client.commit.message,
+        time: client.commit.author.date,
+      },
+      server: {
+        committer: server.commit.author.name,
+        message: server.commit.message,
+        time: server.commit.author.date,
+      },
+    };
+    return report;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function sendReportToDiscord(report) {
+  console.log(process.env.NODE_ENV, report);
+}
+
+async function init() {
+  const report = await getReportFromLatestCommit();
+
+  sendReportToDiscord(report);
   await services.init();
   await books.init();
 }
-
 module.exports = { init };
