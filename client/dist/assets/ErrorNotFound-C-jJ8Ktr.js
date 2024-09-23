9cddf4ad9bcbb10486a8af9a1b7493bd6fb4629b
Augment Chinedu <augmentchinedu@gmail.com>
2024-09-23 18:07:55 +0100

    New Build. Footer Logout Optimized

--- client/dist/assets/ErrorNotFound-C-jJ8Ktr.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as r,j as t,z as e,h as n}from"./index-C_od0Hn2.js";const c={},a=n("br",null,null,-1);function s(_,l){return r(),t("div",null,[e("Error 404 "),a,e("page not found")])}const u=o(c,[["render",s]]);export{u as default};
--- client/dist/assets/Index-BQ8dM9p6.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c,j as s}from"./index-C_od0Hn2.js";const o={},t={id:"chess"};function r(n,_){return c(),s("div",t,"Chess Game")}const f=e(o,[["render",r]]);export{f as default};
